7ca93863dfa11f0a54a36992e9d907b65d88b475
Saul-Galvan-Alvarado-1999 <saul.galvan@utxicotepec.edu.mx>
2021-07-18 13:32:20 -0500

    Cambios sobre home y actualización de imagen, v.1.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,292 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="card bg-dark text-white" >
+  <img src="http://localhost/le_lavage/public/img/fondo3.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+  
+<br><br><br>
+<br>
+    <h1 style="font-size: 150px;text-align: center;font-family:Lato;font-weight:900;font-style:italic" class="vc_custom_heading">
+Le Lavage...</h1>
+<h1 style="font-size: 30px;text-align: center;font-family:Lato;font-weight:900;font-style:italic" class="vc_custom_heading">
+Por que un carro limpio dice más que mil palabras sobre su conductor...
+    Somos profesionales en el lavado de autos!!!</h1>
+    <p class="card-text"></p>
+    
+  </div>
+</div>  
+
+<br><br><br>
+<h1 style="font-size: 30px;text-align: center;font-family:Lato;font-weight:900;font-style:italic" class="vc_custom_heading">
+Tipo de Vehículos con los que Trabajamos</h1>
+<br><br><br>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header" style="background-color:#87CEFA">{{ __('Bienvenido') }}</div>
+    <div class="row">
+      <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/moto3.jpg" width="420" height="290">
+ 
+    <h4 class="card-title" align="center">Motocicleta</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
+      <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/motoneta2.jpg" width="420" height="290">
+ 
+    <h4 class="card-title" align="center">Motoneta</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
+       <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/cuatri.png" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Cuatri Moto</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
+           <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/car1.jpg" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Automóvil</h4>
+    <p class="card-text">.</p>
+  </div>
+          </div>
+           <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/car3.jpg" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Automóvil Chico</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
+           <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/combi.jpg" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Combi</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
+           <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/troca.jpg" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Camionetas</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
 
-                <div class="card-body" style="background-color:#DCDCDC">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Estas en el Sistema de control de el autolavado "Le Lavage", en la parte 
-                    superior podras encontrar un menú en el cual se encuentran todas las áreas
-                    pertenecientes a este Sistema, en cada una de ellas podras visualizar, crear, modificar y
-                     eliminar (tareas básicas) de acuerdo a lo que requieras hacer') }}
-                </div>
-            </div>
-        </div>
+          <div class="col">
+      <div class="card" style="width:420px">
+  <img  src="http://localhost/le_lavage/public/img/troca2.jpg" width="420" height="280">
+ 
+    <h4 class="card-title" align="center">Camionetas Cerradas</h4>
+    <p class="card-text"></p>
+  </div>
+          </div>
     </div>
+  <br><br><br>
+  <h1 style="font-size: 30px;text-align: center;font-family:Lato;font-weight:900;font-style:italic" class="vc_custom_heading">
+Servicios</h1>
+<br><br><br>
+<div class="container-fluid">
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+DISTINGUIDO
+</div>
+<div class="card-block">
+<h3 class="card-title"></h3>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left">Pre-lavado</li>
+<li align="left">Shampoo</li>
+<li align="left">Acondicionador + Cepillado de Llantas y Rines</li>
+<li align="left">Secado a mano con cera líquida</li>
+<li align="left">Acondicionador en Llantas</li>
+<li align="left">Limpieza de marcos de puertas</li>
+<br>
+<br><br>
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$100.00
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+ELEGANTE
+</div>
+<div class="card-block " style="background-color:#87CEFA">
+<h4 class="card-title"></h4>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left">Pre-lavado</li>
+<li align="left">Shampoo</li>
+<li align="left">Lavado Chasis</li>
+<li align="left">Secado a mano con cera líquida</li>
+<li align="left">Acondicionador + Cepillado de Llantas y Rines</li>
+<li align="left">Limpieza y Aspirado Interior</li>
+<li align="left">Acondicionador en Llantas</li>
+<li align="left">Aromatizante</li>
+<br>
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$130.00
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+GLAMOUR
+</div>
+<div class="card-block " style="background-color:#6594EB">
+<h4 class="card-title"></h4>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left">Pre-lavado</li>
+<li align="left">Shampoo TRICOLOR (Brillo total en pintura)</li>
+<li align="left">Teflón protector</li>
+<li align="left">Lavado de Chasis</li>
+<li align="left">Acondicionador + Capillado de Llantas y Rines</li>
+<li align="left">Secado a mano con cera líquida</li>
+<li align="left">Acondicionador en Interiores + Vistas</li>
+<li align="left">Acondicionador en Llantas</li>
+<li align="left">Aromatizante</li>
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$150.00
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 
+<br>
+<div class="container-fluid">
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+MOTO CLEAN
+</div>
+<div class="card-block bg-dark">
+<h3 class="card-title"></h3>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left" style="color:white;">Lavado de moto</li>
+<li align="left" style="color:white;">Desengrasado de Escape</li>
+<li align="left" style="color:white;">Abrillantado de Plásticos</li>
+<br>
+<br><br>
+
+<br><br><br>
+
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$50.00
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+MOTO SUPER CLEAN
+</div>
+<div class="card-block bg-secondary">
+<h4 class="card-title"></h4>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left" style="color:white;">Lavado de moto</li>
+<li align="left" style="color:white;">Encerado de moto</li>
+<li align="left" style="color:white;">Pulido de faros</li>
+<li align="left" style="color:white;">Desengrasado de tolvas, motor y escape</li>
+<li align="left" style="color:white;">Limpieza de frenos</li>
+<li align="left" style="color:white;">Lubricado de cadena</li>
+<li align="left" style="color:white;"> Abrillantado de plásticos, piel y cromos</li>
+<br><br>
+
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$140.00
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="card text-center">
+<div class="card-header">
+MOTO ESTÉTICO ULTRA CLEAN
+</div>
+<div class="card-block " >
+<h4 class="card-title"></h4>
+<p class="card-text">
+<div class="wpb_wrapper" >
+<ul>
+<li align="left">Lavado de moto</li>
+<li align="left">Descontaminación de pintura y cromos</li>
+<li align="left">Pulido de pintura y cromos</li>
+<li align="left">Sellado y abrillantado de pintura y cromos</li>
+<li align="left">Pulido de faros</li>
+<li align="left">Desengrasado de tolvas, motor y escape</li>
+<li align="left">Limpieza de frenos</li>
+<li align="left">Lubricado de cadena</li>
+<li align="left">Abrillantado de plásticos, piel y cromos</li>
+</ul>
+</div> 
+</p>
+</div>
+<div class="card-footer text-muted">
+$450.00
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+    <br>
+
+
+<br><br><br>
+<center>
+        <div class="wow fadeInUp col-md-12" data-wow-delay="0.4s">
+            <h1 style="font-size: 30px;text-align: center;font-family:Lato;font-weight:900;font-style:italic" class="vc_custom_heading">
+            Nos ubicamos en</h1><br><br><br>
+            <div id="google-map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14974.078514299827!2d-97.96117317770992!3d20.237273231189246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d08602491e37a1%3A0x4bbea4aede57d500!2sUniversidad%20Tecnol%C3%B3gica%20de%20Xicotepec%20de%20Ju%C3%A1rez!5e0!3m2!1ses-419!2smx!4v1610957049344!5m2!1ses-419!2smx" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </div>
+        </div>
+    </center>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" >
-                <img  src="../img/logo.png" width="100" height="50" >
+                <img  src="http://localhost/le_lavage/public/img/logo.png" width="100" height="50" >
                     
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -100,8 +100,10 @@
     </div>
 </body>
 <footer class="footer">
+<div class="container-fluid">
+<div class="container bg-light">
 <div class="footer-copyright text-center py-3" class="footer__p" position: fixed;>© 2021 Copyright, Xicotepec de Juárez Puebla
       <a href="https://mdbootstrap.com/education/bootstrap/"> </a>
-    </div>
+    </div></div></div>
     </footer>
 </html>
